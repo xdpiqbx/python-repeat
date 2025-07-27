9770fa980ec296417593390b605c6fb71301945f
xdpiqbx <xdpiqbx@gmail.com>
2025-07-27 19:39:58 +0300

    sec 27

---
+++ 018_operators.py
@@ -0,0 +1,26 @@
+# Section 27 https://www.udemy.com/course/python-ru/learn/lecture/35368740#overview
+
+# Logic
+# ==, !=, <, >
+# not, and, or
+
+# Unary
+# + (num to positive)
+# - (num to negative)
+# not
+
+# Binary
+# =
+# +
+# +=
+# ==
+# and
+
+# For objects (is in collection, in object)
+# in
+# not in
+# is
+# is not
+
+# priority for:
+# +, -, *, /, %
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ python --version
 
 https://docs.python.org/uk/3.11/library/functions.html
 
-Section 26
+Section 27
 
-https://www.udemy.com/course/python-ru/learn/lecture/35368730#overview
+https://www.udemy.com/course/python-ru/learn/lecture/35368740#overview
