12811669980249e2e2a6bfa1fc8116e2b286fe14
xdpiqbx <xdpiqbx@gmail.com>
2025-07-27 19:44:14 +0300

    sec 28 done

--- 006_bool.py
+++ 006_bool.py
@@ -3,8 +3,10 @@ print(bool("abc"))  # True
 print(bool([1, 2]))  # True
 print(bool(""))  # False
 print(bool(None))  # False
-print(bool([]))  # False
-print(bool({}))  # False
-print(bool(()))  # False
+print(bool([]))  # False list - []
+print(bool({}))  # False dict - {} (empty dict)
+print(bool(()))  # False tuple - ()
+print(bool(set()))  # False set - {}
+print(bool(range(0)))  # False
 
 print(bool(False < True))  # True
